cce43ca00010ed47c561420523752d7d9e77b9ad
Artem Ibragimov <artem.ibragimov1602@gmail.com>
2022-12-04 18:07:49 +0300

    Initial commit

---
+++ include/bmp.h
@@ -0,0 +1,6 @@
+#ifndef bmp_h
+#define bmp_h
+
+#include <stdio.h>
+
+#endif /* bmp_h */
---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "bmp.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
