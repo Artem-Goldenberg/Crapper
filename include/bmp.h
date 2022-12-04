e8a9322ae087f6f85a8d8a53725f4926c72d6152
Artem Ibragimov <artem.ibragimov1602@gmail.com>
2022-12-04 19:57:20 +0300

    Argument validation + bmp functions declaration

--- include/bmp.h
+++ include/bmp.h
@@ -1,6 +1,37 @@
 #ifndef bmp_h
 #define bmp_h
 
-#include <stdio.h>
+#include <stdint.h>
+
+typedef struct {
+    int x, y, w, h;
+} Rect;
+
+/**
+ Load given bmp file with specifyed width and height
+ 
+ All pixel values will be written in this array in format 0xAABBGGRR
+ 
+ where     
+ 
+ AA - 8 bit space (currently unused),    
+ 
+ BB - 8 bit space for blue component,   
+ 
+ GG - 8 bit space for green component,   
+ 
+ RR - 8 bit space for red component    
+ 
+ - Parameter pixels: 2D array with predefined size where values of all read pixels will be stored
+ 
+ - Returns: 0 if file was successfully loaded, 1 if the error occured
+ */
+int loadBmp(int width, int height, uint32_t pixels[height][width], const char *filename);
+
+void crop(int width, int height, uint32_t pixels[height][width], Rect rect);
+
+void rotate(int width, int height, uint32_t pixels[height][width]);
+
+int saveBmp(int width, int height, uint32_t pixels[height][width], const char *filename);
 
 #endif /* bmp_h */
--- src/main.c
+++ src/main.c
@@ -1,8 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 #include "bmp.h"
 
+#define NARGS 8
+#define BADARGS do { printUsage(); return 1; } while(0)
+
+void printUsage(void) {
+    puts("Usage: bin/hw_01 crop-rotate ‹in-bmp› ‹out-bmp› ‹x› ‹y› ‹w› ‹h›");
+}
+
+/**
+ Assuming number of args is correct, validates them and in case of no errors fill the `Rect` struct with valid origin and size values.
+ 
+ `rect` will have valid information only if function returns 0, otherwise the contents of this struct is undefined
+ 
+ - Parameter argv: Array of string arguments, its size is assumed to be `NARGS`
+ 
+ - Parameter rect: Pointer to a `Rect` struct, which will be initialized by this function
+ 
+ - Returns: 0 if there was no error parsing args, 1 otherwise
+ */
+int validateArgs(const char * argv[], Rect* rect) {
+    if (strcmp(argv[1], "crop-rotate") != 0) BADARGS;
+    
+    const char* inputFilename = argv[2];
+    const char* outputFilename = argv[3];
+    
+    rect->x = atoi(argv[4]);
+    rect->y = atoi(argv[5]);
+    rect->w = atoi(argv[6]);
+    rect->h = atoi(argv[7]);
+    
+    return 0;
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    if (argc != NARGS) BADARGS;
+    
+    Rect rect;
+    if (validateArgs(argv, &rect) != 0) return 1;
+    
     return 0;
 }
